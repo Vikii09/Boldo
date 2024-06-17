8329d145fd13674afa541859d859346c535dc9c7
Vikii09 <167336442+Vikii09@users.noreply.github.com>
2024-06-17 12:18:32 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,103 @@
+$('.header-bottom__one').animate({
+    "width": "0px"
+}).animate({
+    "width": "270px",
+});
+
+$('.header-bottom__line--two').animate({
+    "width": "0px"
+}).animate({
+    "width": "80px"
+});
+
+$('.header-bottom__line--three').animate({
+    "width": "0px"
+}).animate({
+    "width": "317px"
+});
+
+$('.header-bottom__line--four').animate({
+    "width": "0px"
+}).animate({
+    "width": "253px"
+});
+
+// STOLBS
+
+$('.header-bottom__stolb--one').animate({
+    "height": "0px"
+}).animate({
+    "height": "23px"
+});
+
+$('.header-bottom__stolb--two').animate({
+    "height": "0px"
+}).animate({
+    "height": "13px"
+});
+
+$('.header-bottom__stolb--three').animate({
+    "height": "0px"
+}).animate({
+    "height": "62px"
+});
+
+$('.header-bottom__stolb--four').animate({
+    "height": "0px",
+}).animate({
+    "height": "43px",
+});
+
+$('.header-bottom__stolb--five').animate({
+    "height": "0px"
+}).animate({
+    "height": "83px"
+});
+
+$('.header-bottom__stolb--six').animate({
+    "height": "0px"
+}).animate({
+    "height": "58px"
+});
+
+$('.header-bottom__stolb--seven').animate({
+    "height": "0px"
+}).animate({
+    "height": "70px"
+});
+
+$('.header-bottom__stolb--aight').animate({
+    "height": "0px"
+}).animate({
+    "height": "31px"
+});
+
+// SLIDER
+
+// $('.slick-next').click(function() {
+//     $('.slider__item-three').css({
+//         "display": "block"
+//     })
+
+//     $('.slider__item-one').css({
+//         "display": "none"
+//     })
+
+//     $('.slider__item-two').css({
+//         "display": "none"
+//     })
+// })
+
+// $('.slick-prev').click(function() {
+//     $('.slider__item-one').css({
+//         "display": "block"
+//     })
+    
+//     $('.slider__item-three').css({
+//         "display": "none"
+//     })
+
+//     $('.slider__item-two').css({
+//         "display": "none"
+//     })
+// })
